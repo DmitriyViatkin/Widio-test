84a02c8065d0ad40da9c82ec5f9278e1368526f8
Dima Viatkin <viatkindima@gmail.com>
2024-02-14 12:06:57 +0200

    start1

---
+++ test/specs/test.e2e.js
@@ -0,0 +1,17 @@
+import { expect } from '@wdio/globals'
+
+describe("Webdriverio main page", () => {
+
+    it("should have correct title", async () => {
+         
+        await browser.url('https://webdriver.io/')
+
+        const title= await browser.getTitle()
+        console.log(title);
+
+        await expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework or Node.js | WebdriverIO')
+
+    });
+
+
+});
